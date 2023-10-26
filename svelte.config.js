c73e2333b7941ade91dbd88c4e815993b62fd793
GeorgeAzma <gg20060705@gmail.com>
2023-10-26 14:54:01 +0400

    fixes

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,8 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({fallback: 'index.html'})
 	}
 };
-
 export default config;
